f2a8c62e1cf56a4b0a369a48784fab8a68b49898
Vasily Parshentsev <vparshentsev@gmail.com>
2023-06-16 14:18:41 +0300

    rework method init

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ru.netology.servlet;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.repository.PostRepository;
 import ru.netology.service.PostService;
@@ -20,9 +22,16 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+//    final var repository = new PostRepository();
+//    final var service = new PostService(repository);
+//    controller = new PostController(service);
+
+    // отдаём класс конфигурации
+    final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+
+    final var repository = context.getBean("postRepository");
+    final var controller = context.getBean("postController");
+    final var service = context.getBean("postService");
   }
 
   @Override
